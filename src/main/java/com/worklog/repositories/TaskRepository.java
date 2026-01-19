f72239dbe7ffccf47bbd3c57982b2cbeac25a96c
Vastpro-5 <Vastpro-5@DESKTOP-RT9V8AK>
2026-01-19 18:18:27 +0530

    -- task repo config set by vasu.

---
+++ src/main/java/com/worklog/repositories/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.worklog.repositories;
+
+import java.util.List;
+
+import com.worklog.entities.Task;
+
+public class TaskRepository {
+
+	public List<Task> getAllTaskDetails() {
+		return null;
+	}
+}
