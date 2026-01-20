0d5a758adecb0760ac0aa2eea9acb8931b11b822
Vastpro-5 <Vastpro-5@DESKTOP-RT9V8AK>
2026-01-20 10:44:23 +0530

    Password util class commit by vasu.

---
+++ src/main/java/com/worklog/utils/PasswordProtector.java
@@ -0,0 +1,27 @@
+package com.worklog.utils;
+
+import java.util.Objects;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * 
+ * PasswordProtector - Utility class used for password hashing and check the hashed password.
+ * 
+ * @author Vasudevan Tamizharasan
+ * @since 20-01-2026
+ */
+
+public class PasswordProtector {
+
+	public static String getHashedPassword(String password) {
+		Objects.requireNonNull(password, "password from user is null.");
+		return BCrypt.hashpw(password, BCrypt.gensalt(10));
+	}
+
+	public static boolean checkPassword(String password, String hashedPassword) {
+		Objects.requireNonNull(password, "password from user is null.");
+		Objects.requireNonNull(hashedPassword, "password from db is null.");
+		return BCrypt.checkpw(password, hashedPassword);
+	}
+}
