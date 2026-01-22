17e7b848c6559ba7f0b885f79e848751bdee5673
preethapadmanaban <padmanaban18600@gmail.com>
2026-01-22 16:15:43 +0530

    command.xml, CreateTaskPageCommand has been committed by preetha

---
+++ src/main/java/com/worklog/commands/CreateTaskPageCommand.java
@@ -0,0 +1,36 @@
+package com.worklog.commands;
+
+import java.util.List;
+
+import com.worklog.entities.Employee;
+import com.worklog.interfaces.Command;
+import com.worklog.repositories.EmployeeDAO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class CreateTaskPageCommand implements Command{
+
+	@Override
+	public boolean execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		HttpSession session = request.getSession(false);
+		
+        if (session == null) {
+        	return false;
+        }
+
+        String role = (String) session.getAttribute("role");
+        
+        if (role == null || !role.equalsIgnoreCase("Manager")) {
+        	 return false;
+        }
+
+        List<Employee> members = EmployeeDAO.getAllMembers();
+        request.setAttribute("Members", members);
+        
+		return true;
+	}
+
+}
